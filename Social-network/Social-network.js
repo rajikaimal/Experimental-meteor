f5c43e81c90c5c496b631fe8fb7ea1241773e956
Rajika <inbox.rajika@gmail.com>
2015-08-31 00:17:39 +0530

    settings

--- Social-network/Social-network.js
+++ Social-network/Social-network.js
@@ -1,12 +1,35 @@
+Users = new Mongo.Collection('Users');
+
 if (Meteor.isClient) {
   Router.route('/register');
 
   Router.route('/login');
 
-  Router.route('/home');  
+  Router.route('/home'); 
+
+  Router.route('/settings'); 
 
   Router.configure({
     layoutTemplate : 'main'
+  });
+
+  Template.settings.helpers({
+    userInfo : function(){
+      console.log(Users.find({ _id : Meteor.userId() }));
+      return Users.findOne({ _id : Meteor.userId() });
+    }
+  });
+
+  Template.settings.events({
+    'submit .settings-form' : function(event){
+      event.preventDefault();
+      var age = event.target.age.value;
+      var hobby = event.target.hobby.value;
+      Users.update( {_id : Meteor.userId()} ,{ $set : { age : age , hobby : hobby }});
+
+      event.target.age.value = "";
+      event.target.hobby.value = "";
+    }
   });
 }
 
