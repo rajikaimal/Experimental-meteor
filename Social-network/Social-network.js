2c1aa87942aea8668dcd51922abd19636e74013a
Rajika <inbox.rajika@gmail.com>
2015-09-02 09:45:33 +0530

    del

--- Social-network/Social-network.js
+++ Social-network/Social-network.js
@@ -1,9 +1,7 @@
 Users = new Mongo.Collection('Users');
+Feed = new Mongo.Collection('Feed');
 
 if (Meteor.isClient) {
-  Router.route('/register');
-
-  Router.route('/login');
 
   Router.route('/home'); 
 
@@ -13,9 +11,30 @@ if (Meteor.isClient) {
     layoutTemplate : 'main'
   });
 
+
+  Template.home.events({
+    'submit .posttowall': function(event){
+      event.preventDefault();
+      var postWall = event.target.posting.value;
+      Feed.insert({ postId : Meteor.userId() , post : postWall });
+    }
+  });
+
+  Template.newsFeed.helpers({
+    feed : function(){
+      return Feed.find({ postId : Meteor.userId() });
+    }
+  });
+
+  Template.newsFeed.events({
+    'click .removePost' : function(event){
+      Feed.remove(this._id);
+    }
+  });
+
   Template.settings.helpers({
     userInfo : function(){
-      console.log(Users.find({ _id : Meteor.userId() }));
+      console.log(Users.find({ postId : Meteor.userId() }));
       return Users.findOne({ _id : Meteor.userId() });
     }
   });
@@ -25,6 +44,10 @@ if (Meteor.isClient) {
       event.preventDefault();
       var age = event.target.age.value;
       var hobby = event.target.hobby.value;
+      if(age == "" || hobby == ""){
+        alert("Fill all");
+      }
+
       Users.update( {_id : Meteor.userId()} ,{ $set : { age : age , hobby : hobby }});
 
       event.target.age.value = "";
